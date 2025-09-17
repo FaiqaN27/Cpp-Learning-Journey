6403cda61061810e433ae86af016835e077d83a5
Faiqa Nadeem <faiqanadeem6725@gmail.com>
2025-09-18 04:04:33 +0500

    pattern 22 completed

--- patterns/22.cpp
+++ patterns/22.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
@@ -10,10 +10,25 @@ int main()
   {
     for (int j = 0; j < 2 * n - 1; j++)
     {
-        }
+      int top = i;
+      int left = j;
+      int bottom = (2 * n - 1) - 1 - j;
+      int right = (2 * n - 1) - 1 - i;
+      cout << n - min(min(top, bottom), min(left, right)) << " ";
+    }
+    cout << endl;
   }
 
   return 0;
 }
 
 // pattern
+// 5 5 5 5 5 5 5 5 5
+// 5 4 4 4 4 4 4 4 5
+// 5 4 3 3 3 3 3 4 5
+// 5 4 3 2 2 2 3 4 5
+// 5 4 3 2 1 2 3 4 5
+// 5 4 3 2 2 2 3 4 5
+// 5 4 3 3 3 3 3 4 5
+// 5 4 4 4 4 4 4 4 5
+// 5 5 5 5 5 5 5 5 5
